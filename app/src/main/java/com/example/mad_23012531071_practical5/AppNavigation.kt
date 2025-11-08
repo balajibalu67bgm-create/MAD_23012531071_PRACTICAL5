3862c2becb088c464523c52e712da75a7147c760
KARNA VENKATA NAGA BALAJI <balajibalu67bgm@gmail.com>
2025-11-08 23:23:37 +0530

    INITIAL COMMIT

---
+++ app/src/main/java/com/example/mad_23012531071_practical5/AppNavigation.kt
@@ -0,0 +1,24 @@
+package com.example.mad_23012531071_practical5
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mad_23012531071_practical5.screens.LoginScreen
+import com.example.mad_23012531071_practical5.screens.RegisterScreen
+
+@Composable
+fun AppNavigation(context: Context){
+    val navcontroller = rememberNavController()
+
+    NavHost(navcontroller, startDestination = "login"){
+        composable("login"){
+            LoginScreen(context, navcontroller)
+        }
+        composable("register"){
+            RegisterScreen(context, navcontroller)
+        }
+
+    }
+}
